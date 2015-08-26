8ac2d6f65d444d46a896d529f3bb25cb6a84b17f
Kevin Townsend <contact AT microbuilder D0T eu>
2015-08-26 02:02:13 +0200

    Added system status helper

--- examples/sensorapi/sensorapi.ino
+++ examples/sensorapi/sensorapi.ino
@@ -5,7 +5,7 @@
 
 /* This driver uses the Adafruit unified sensor library (Adafruit_Sensor),
    which provides a common 'type' for sensor data and some helper functions.
-   
+
    To use this driver you will also need to download the Adafruit_Sensor
    library and include it in your libraries folder.
 
@@ -14,14 +14,14 @@
    sensor in any data logs, etc.  To assign a unique ID, simply
    provide an appropriate value in the constructor below (12345
    is used by default in this example).
-   
+
    Connections
    ===========
    Connect SCL to analog 5
    Connect SDA to analog 4
    Connect VDD to 3-5V DC
    Connect GROUND to common ground
-    
+
    History
    =======
    2015/MAR/03  - First release (KTOWN)
@@ -29,7 +29,7 @@
 
 /* Set the delay between fresh samples */
 #define BNO055_SAMPLERATE_DELAY_MS (100)
-   
+
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
 /**************************************************************************/
@@ -48,8 +48,31 @@ void displaySensorDetails(void)
   Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
   Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" xxx");
   Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" xxx");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" xxx");  
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" xxx");
   Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+}
+
+/**************************************************************************/
+/*
+    Display some basic info about the sensor status
+*/
+/**************************************************************************/
+void displaySensorStatus(void)
+{
+  /* Get the system status values (mostly for debugging purposes) */
+  uint8_t system_status, self_test_results, system_error;
+  system_status = self_test_results = system_error = 0;
+  bno.getSystemStatus(&system_status, &self_test_results, &system_error);
+
+  /* Display the results in the Serial Monitor */
+  Serial.print("System Status: 0x");
+  Serial.println(system_status, HEX);
+  Serial.print("Self Test:     0x");
+  Serial.println(self_test_results, HEX);
+  Serial.print("System Error:  0x");
+  Serial.println(system_error, HEX);
   Serial.println("");
   delay(500);
 }
@@ -59,11 +82,11 @@ void displaySensorDetails(void)
     Arduino setup function (automatically called at startup)
 */
 /**************************************************************************/
-void setup(void) 
+void setup(void)
 {
   Serial.begin(9600);
   Serial.println("Orientation Sensor Test"); Serial.println("");
-  
+
   /* Initialise the sensor */
   if(!bno.begin())
   {
@@ -71,11 +94,14 @@ void setup(void)
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
     while(1);
   }
-  
+
   delay(1000);
-    
+
   /* Display some basic information on this sensor */
   displaySensorDetails();
+
+  /* Optional: Display current status */
+  // displaySensorStatus();
 
   bno.setExtCrystalUse(true);
 }
@@ -86,12 +112,12 @@ void setup(void)
     should go here)
 */
 /**************************************************************************/
-void loop(void) 
+void loop(void)
 {
-  /* Get a new sensor event */ 
-  sensors_event_t event; 
+  /* Get a new sensor event */
+  sensors_event_t event;
   bno.getEvent(&event);
-  
+
   /* Display the floating point data */
   Serial.print("X: ");
   Serial.print(event.orientation.x, 4);
@@ -100,6 +126,6 @@ void loop(void)
   Serial.print("\tZ: ");
   Serial.print(event.orientation.z, 4);
   Serial.println("");
-  
+
   delay(BNO055_SAMPLERATE_DELAY_MS);
 }
