edb50909f486b86b96e7e9188e191f3f311e33f4
Saulo <sauloal@gmail.com>
2016-03-03 22:01:44 +0100

    Update Adafruit_BNO055.h

--- Adafruit_BNO055.h
+++ Adafruit_BNO055.h
@@ -33,8 +33,8 @@
  #include <Wire.h>
 #endif
 
-#include "Adafruit_Sensor.h"
-#include "utility/imumaths.h"
+#include <Adafruit_Sensor.h>
+#include <utility/imumaths.h>
 
 #define BNO055_ADDRESS_A (0x28)
 #define BNO055_ADDRESS_B (0x29)
