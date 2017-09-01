6d10366deb25c67093ccd4e3086d2373bb27aa60
Thomas Bruen <tom.bruen@gmail.com>
2017-09-01 10:19:48 +0100

    added rotation vector to print function

--- examples/ReadAllData/ReadAllData.ino
+++ examples/ReadAllData/ReadAllData.ino
@@ -71,7 +71,7 @@ void loop(void)
 void printEvent(sensors_event_t* event) {
   Serial.println();
   Serial.print(event->type);
-  double x = -1000000,y = -1000000 , z = -1000000; //dumb values, easy to spot problem
+  double x = -1000000, y = -1000000 , z = -1000000; //dumb values, easy to spot problem
   if (event->type == SENSOR_TYPE_ACCELEROMETER) {
     x = event->acceleration.x;
     y = event->acceleration.y;
@@ -87,7 +87,7 @@ void printEvent(sensors_event_t* event) {
     y = event->magnetic.y;
     z = event->magnetic.z;
   }
-  else if (event->type == SENSOR_TYPE_GYROSCOPE) {
+  else if ((event->type == SENSOR_TYPE_GYROSCOPE) || (event->type == SENSOR_TYPE_ROTATION_VECTOR)) {
     x = event->gyro.x;
     y = event->gyro.y;
     z = event->gyro.z;
