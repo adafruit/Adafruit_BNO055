d71199a8c6b20c75ea98739061cf6101d84367bd
Kevin Townsend <contact@microbuilder.eu>
2018-01-11 14:03:34 +0100

    Set cal typedef to signed values

--- Adafruit_BNO055.h
+++ Adafruit_BNO055.h
@@ -44,18 +44,18 @@
 
 typedef struct
 {
-    uint16_t accel_offset_x;
-    uint16_t accel_offset_y;
-    uint16_t accel_offset_z;
-    uint16_t gyro_offset_x;
-    uint16_t gyro_offset_y;
-    uint16_t gyro_offset_z;
-    uint16_t mag_offset_x;
-    uint16_t mag_offset_y;
-    uint16_t mag_offset_z;
+    int16_t accel_offset_x;
+    int16_t accel_offset_y;
+    int16_t accel_offset_z;
+    int16_t gyro_offset_x;
+    int16_t gyro_offset_y;
+    int16_t gyro_offset_z;
+    int16_t mag_offset_x;
+    int16_t mag_offset_y;
+    int16_t mag_offset_z;
 
-    uint16_t accel_radius;
-    uint16_t mag_radius;
+    int16_t accel_radius;
+    int16_t mag_radius;
 } adafruit_bno055_offsets_t;
 
 class Adafruit_BNO055 : public Adafruit_Sensor
