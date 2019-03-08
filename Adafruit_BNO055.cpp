a7b2a1c91e7e0bd5317f6064dfa6701b4a54e429
Jan Hoffmann <jan@hfmn.pl>
2019-03-08 15:03:38 +0100

    power management functions from PR#57
    enterSuspentMode + enterNormalMode

--- Adafruit_BNO055.cpp
+++ Adafruit_BNO055.cpp
@@ -710,6 +710,36 @@ bool Adafruit_BNO055::isFullyCalibrated() {
 }
 
 /*!
+ *  @brief  Enter Suspend mode (i.e., sleep)
+ */
+void Adafruit_BNO055::enterSuspendMode() {
+  adafruit_bno055_opmode_t modeback = _mode;
+
+  /* Switch to config mode (just in case since this is the default) */
+  setMode(OPERATION_MODE_CONFIG);
+  delay(25);
+  write8(BNO055_PWR_MODE_ADDR, 0x02);
+  /* Set the requested operating mode (see section 3.3) */
+  setMode(modeback);
+  delay(20);
+}
+
+/*!
+ *  @brief  Enter Normal mode (i.e., wake)
+ */
+void Adafruit_BNO055::enterNormalMode() {
+  adafruit_bno055_opmode_t modeback = _mode;
+
+  /* Switch to config mode (just in case since this is the default) */
+  setMode(OPERATION_MODE_CONFIG);
+  delay(25);
+  write8(BNO055_PWR_MODE_ADDR, 0x00);
+  /* Set the requested operating mode (see section 3.3) */
+  setMode(modeback);
+  delay(20);
+}
+
+/*!
  *  @brief  Writes an 8 bit value over I2C
  */
 bool Adafruit_BNO055::write8(adafruit_bno055_reg_t reg, byte value) {
