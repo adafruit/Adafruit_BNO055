7e2b43afa19ad59e04099a6628df8ba37e5777b6
Gintaras <combinacijus@gmail.com>
2019-07-01 19:42:22 +0300

    Update read_all_data.ino

--- examples/read_all_data/read_all_data.ino
+++ examples/read_all_data/read_all_data.ino
@@ -32,7 +32,7 @@ uint16_t BNO055_SAMPLERATE_DELAY_MS = 100;
 
 // Check I2C device address and correct line below (by default address is 0x29 or 0x28)
 //                                   id, address
-Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x29);
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
 
 void setup(void)
 {
