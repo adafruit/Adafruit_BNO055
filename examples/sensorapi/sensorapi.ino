626813608e52c6c129390b2c853fa199993d5fba
Gintaras <combinacijus@gmail.com>
2019-07-01 19:42:52 +0300

    Update sensorapi.ino

--- examples/sensorapi/sensorapi.ino
+++ examples/sensorapi/sensorapi.ino
@@ -33,7 +33,7 @@
 
 // Check I2C device address and correct line below (by default address is 0x29 or 0x28)
 //                                   id, address
-Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x29);
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
 
 /**************************************************************************/
 /*
